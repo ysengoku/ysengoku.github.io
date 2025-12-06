8023ab7c5fc300d885df030cc1727ba0b3f0e574
ysengoku <ysengoku.tourisme@gmail.com>
2025-12-06 21:11:33 +0100

    ops: add import alias, clean up unused files

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@types': path.resolve(__dirname, './src/types'),
+    },
+  },
   plugins: [vue(), tailwindcss()],
 })
